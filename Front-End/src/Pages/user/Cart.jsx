27a30b4494ae6583b6d38a4341d0d13dd951ff59
keerthirajamani31 <keerthanaanu3103@gmail.com>
2025-11-12 18:30:23 +0530

    Fixed mobile reponsive for authentication

--- Front-End/src/Pages/user/Cart.jsx
+++ Front-End/src/Pages/user/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+ import React, { useState, useEffect } from 'react';
 import { FaTrash, FaPlus, FaMinus, FaShoppingCart, FaArrowLeft, FaUser } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 
